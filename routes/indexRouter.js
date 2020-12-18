5f0d77297774d80121c01562d34af307323129ef
nikolaVlad <nikolavlad63@gmail.com>
2020-12-18 21:44:27 +0100

    Razdvojeni MVC fajlovi za rutu index

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,9 +5,7 @@ var router = express.Router();
 const indexController = require('../controllers/indexController');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Web oglasi za posao' });
-});
+router.get('/', indexController);
 
 
 
